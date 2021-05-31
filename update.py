434866b111712678cd93da770a6350d7cb9a1568
FedeClaudi <federicoclaudi@protonmail.com>
2021-05-31 11:26:34 +0100

    new website

--- update.py
+++ update.py
@@ -4,3 +4,12 @@ cv = CV(WIDTH=180)
 cv.to_html('cv.html')
 cv.to_html('me.html')
 cv.to_html('index.html')
+
+'''
+# TODO add to html>body
+    zoom:80%;
+    -moz-transform: scale(.8);
+    position: absolute;
+    top: -200px
+
+'''
